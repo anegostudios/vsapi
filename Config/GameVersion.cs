c04befe52a4021e3c4d6fce040ff76cd70885dde
Tyron <tyronx@gmail.com>
2023-04-17 17:24:40 +0300

    Update to 1.18.0-rc.8

--- Config/GameVersion.cs
+++ Config/GameVersion.cs
@@ -41,7 +41,7 @@ namespace Vintagestory.API.Config
         /// <summary>
         /// Version number in the format: major.minor.revision[appendix]
         /// </summary>
-        public const string ShortGameVersion = OverallVersion + "-rc.7";
+        public const string ShortGameVersion = OverallVersion + "-rc.8";
 
         public static EnumReleaseType ReleaseType => GetReleaseType(ShortGameVersion);
 
