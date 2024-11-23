bb2e87280e010996b87ac4f930a499fb2748a690
pizza2004 <pizza29965@neejean.org>
2024-11-22 20:43:33 -0700

    Add Method to Set Custom Insulation

--- Common/Collectible/Block/Block.cs
+++ Common/Collectible/Block/Block.cs
@@ -1937,15 +1937,25 @@ namespace Vintagestory.API.Common
             {
                 if (type == EnumRetentionType.Sound) return 10;
 
-                var mat = GetBlockMaterial(api.World.BlockAccessor, pos);
-                if (mat == EnumBlockMaterial.Ore || mat == EnumBlockMaterial.Stone || mat == EnumBlockMaterial.Soil || mat == EnumBlockMaterial.Ceramic)
-                {
-                    return -1;
-                }
-                return 1;
+                return Insulation(pos);
             }
 
             return 0;
+        }
+
+        /// <summary>
+        /// Return an integer for use in Heat Retention calculations.
+        /// </summary>
+        /// <param name="pos"></param>
+        /// <returns></returns>
+        public virtual int Insulation(BlockPos pos)
+        {
+            var mat = GetBlockMaterial(api.World.BlockAccessor, pos);
+            if (mat == EnumBlockMaterial.Ore || mat == EnumBlockMaterial.Stone || mat == EnumBlockMaterial.Soil || mat == EnumBlockMaterial.Ceramic)
+            {
+                return -1;
+            }
+            return 1;
         }
 
         public virtual bool IsClimbable(BlockPos pos)
